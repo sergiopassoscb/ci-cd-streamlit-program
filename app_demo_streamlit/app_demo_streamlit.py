667aaa04e637bc8f2f9bb9c074e20b3a27d8aabd
Sérgio Passos <sergio.passos@clickbus.com>
2023-03-16 18:06:04 -0300

    Altera background

--- app_demo_streamlit/app_demo_streamlit.py
+++ app_demo_streamlit/app_demo_streamlit.py
@@ -1,8 +1,16 @@
 import streamlit as st
 
 def main():
+    st.set_page_config(page_title="Minha Aplicação", page_icon=":rocket:", layout="wide", initial_sidebar_state="expanded", background_color="#7d147d")
     st.title("Minha Aplicação Streamlit Alterado")
     st.write("A aplicação foi atualizada")
     
+    
 if __name__ == '__main__':
     main()
+
+
+import streamlit as st
+
+# Define a cor de fundo da página como amarelo
+
