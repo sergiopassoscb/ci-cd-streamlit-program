2d2175495579c300fdccca8e54135af90ae6db4d
Sérgio Passos <sergio.passos@clickbus.com>
2023-03-17 09:45:49 -0300

    Atualização do script

--- app_demo_streamlit/app_demo_streamlit.py
+++ app_demo_streamlit/app_demo_streamlit.py
@@ -4,14 +4,6 @@ import numpy as np
 
 def main():
     st.title("Gráfico de Linha")
-    
-    x = np.linspace(0, 20, 100)
-    y = np.sin(x)
-    
-    fig, ax = plt.subplots()
-    ax.plot(x, y)
-    
-    st.pyplot(fig)
 
 if __name__ == "__main__":
     main()
