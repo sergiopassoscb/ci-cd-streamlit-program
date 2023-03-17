90ed049bfdbef95d056137d0cc4d3d7820a6a691
Sérgio Passos <sergio.passos@clickbus.com>
2023-03-17 08:35:48 -0300

    Alteração da aplicação

--- app_demo_streamlit/app_demo_streamlit.py
+++ app_demo_streamlit/app_demo_streamlit.py
@@ -1,28 +1,17 @@
 import streamlit as st
-import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
 
 def main():
-    st.title("Minha Aplicação Streamlit Alterado")
-    st.write("A aplicação foi atualizada")
+    st.title("Gráfico de Linha")
     
-    # Cria um botão para carregar os dados
-    if st.button("Carregar Dados"):
-        # Cria um dataframe com dados de teste
-        data = {
-            'Nome': ['João', 'Maria', 'Pedro', 'Ana'],
-            'Idade': [25, 30, 27, 22],
-            'Cidade': ['São Paulo', 'Rio de Janeiro', 'Belo Horizonte', 'Porto Alegre']
-        }
-        df = pd.DataFrame(data)
-        # Mostra o dataframe na página
-        st.write(df)
+    x = np.linspace(0, 20, 100)
+    y = np.sin(x)
     
-    # Cria uma caixa de seleção para filtrar os dados
-    cidade_selecionada = st.selectbox("Selecione uma cidade", df['Cidade'].unique())
-    # Filtra o dataframe de acordo com a cidade selecionada
-    df_filtrado = df[df['Cidade'] == cidade_selecionada]
-    # Mostra o dataframe filtrado na página
-    st.write(df_filtrado)
+    fig, ax = plt.subplots()
+    ax.plot(x, y)
     
-if __name__ == '__main__':
+    st.pyplot(fig)
+
+if __name__ == "__main__":
     main()
