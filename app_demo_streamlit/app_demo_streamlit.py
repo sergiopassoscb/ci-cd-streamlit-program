fce57f49e4aa8d853c66b86b94828c5b231b2d2a
michelmiranda-clickbus <80708306+michelmiranda-clickbus@users.noreply.github.com>
2023-05-15 14:14:06 -0300

    Update app_demo_streamlit.py
    testing

--- app_demo_streamlit/app_demo_streamlit.py
+++ app_demo_streamlit/app_demo_streamlit.py
@@ -1,11 +1,21 @@
 import streamlit as st
+import awswrangler as wr
 
 def main():
     st.title("Streamlit com Argo e Kubernetes")
-    st.write("Olá!")
-    st.write("Nova linha!")
-    st.write("Olá Samyro!")
-    st.write("Olá Pessoas!")
+    
+    query = '''
+        select 
+	        store, slug, platform, title
+        from dw_bs.dm_client_application 
+        limit 10;
+    '''
+    df = wr.athena.read_sql_query(sql=query,  database='dw_bs')
+    st.write(df)
+    
+#     st.write("Nova linha!")
+#     st.write("Olá Samyro!")
+#     st.write("Olá Pessoas!")
     
 if __name__ == '__main__':
     main()
